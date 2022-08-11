1235af42092cb86bf92b89a6cd529d8814a03f8a
Piper McCorkle <contact@piperswe.me>
2022-08-10 20:14:57 -0500

    Add DNS records for landing

--- project_landing.tf
+++ project_landing.tf
@@ -10,3 +10,19 @@ resource "github_repository" "project_landing" {
   has_projects  = true
   has_wiki      = true
 }
+
+resource "cloudflare_record" "project_landing_root" {
+  zone_id = cloudflare_zone.blinn_center.id
+  type    = "CNAME"
+  name    = "@"
+  value   = "blinn-center-landing.pages.dev"
+  proxied = true
+}
+
+resource "cloudflare_record" "project_landing_www" {
+  zone_id = cloudflare_zone.blinn_center.id
+  type    = "CNAME"
+  name    = "www"
+  value   = "blinn-center-landing.pages.dev"
+  proxied = true
+}
