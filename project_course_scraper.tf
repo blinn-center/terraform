3366bc1ecf9da0ed613f3da922994ddd1b80278b
Piper McCorkle <contact@piperswe.me>
2022-08-10 22:50:39 -0500

    Create a GitHub repo for the course scraper

---
+++ project_course_scraper.tf
@@ -0,0 +1,11 @@
+resource "github_repository" "project_course_scraper" {
+  name        = "course-scraper"
+  description = "A scraper for Blinn College's Dynamic Schedule"
+
+  visibility = "public"
+
+  has_downloads = false
+  has_issues    = true
+  has_projects  = true
+  has_wiki      = true
+}
